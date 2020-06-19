b49f3e9f28835b371ff887eb9ed6ad989121bb8b
Andrew Fasano <fasano@mit.edu>
2020-06-18 22:39:35 -0400

    WIP: record values used in tainted comparisons, not really working yet

--- panda/plugins/taint2/llvm_taint_lib.h
+++ panda/plugins/taint2/llvm_taint_lib.h
@@ -117,9 +117,9 @@ private:
     void insertTaintMix(Instruction &I, Value *src);
     void insertTaintMix(Instruction &I, Value *dest, Value *src);
     void insertTaintCompute(Instruction &I,
-            Value *src1, Value *src2, bool is_mixed);
+            Value *src1, Value *src2, bool is_mixed, bool is_cmp);
     void insertTaintCompute(Instruction &I, Value *dest,
-            Value *src1, Value *src2, bool is_mixed);
+            Value *src1, Value *src2, bool is_mixed, bool is_cmp);
     void insertTaintMul(Instruction &I, Value *dest,
             Value *src1, Value *src2);
     void insertTaintSext(Instruction &I, Value *src);
@@ -158,6 +158,9 @@ public:
 
     Constant *memlogConst;
     Function *memlogPopF;
+
+    Function *logCmpF;
+    Function *nopF;
 
     Constant *llvConst;
     Constant *memConst;
